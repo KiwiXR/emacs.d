6573c105dec85d86aa557c047f99765a56f7107f
KiwiXR <kiwi2364261522@outlook.com>
2025-01-14 17:10:07 +0800

    update

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,6 +7,12 @@
 ;; use advance-words-count.el to summarize chinese writing statistics
 (add-to-list 'load-path "~/.emacs.d/elpa/advance-words-count.el")
 (require 'advance-words-count)
+(defun nano-modeline-cursor-position (&optional format)
+  "Cursor position using given FORMAT."
+  (let ((format (or format "%l:%c ")))
+    (propertize (format-mode-line '( (:eval (format "%s字" (+ (words-count (point-min) (point-max) words-count-rule-ansci) (words-count (point-min) (point-max) words-count-rule-CJK)))) "   "))
+                'face (nano-modeline-face 'secondary))
+    ))
 
 ;; browse-url leveage from https://www.reddit.com/r/emacs/comments/i0f92n/wsl2_orgprotocol/
 (when (and (eq system-type 'gnu/linux)
